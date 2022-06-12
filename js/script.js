4a6faacdaa70523df26dcbf1498bb9d5b1f1ca2c
Tushardatta7 <tushardatta51@gmailcom>
2022-06-12 12:08:11 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// var x;
+// x = prompt("Enter your Name: ");
+// console.log(x);
+// document.write(x);
+
+// const a=100;
+// document.write(x);
+// a=200;
+
+var temp = prompt("Please enter an tempreture:");
+
+var result = 9/5*temp + 32;
+alert("Farhrenheit: " + result + "Degree");
+
+console.log("Tesk complete");
+var student = {name: "Rahim", age:25, hometown:"Dhaka"}
